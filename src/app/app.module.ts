6628a74d3f28919fc8f4339c44146e2ef637c183
Luca Guarro <luca.guarro@gmail.com>
2017-06-11 23:01:26 -0700

    layed out form page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { VideoMakerComponent } from './video-maker/video-maker.component';
 import { VideoMakerPageComponent } from './video-maker-page/video-maker-page.component';
 import { ResultPageComponent } from './result-page/result-page.component';
 import { ResultVideoComponent } from './result-page/result-video/result-video.component';
@@ -15,7 +14,6 @@ import { PreviewComponent } from './video-maker-page/preview/preview.component';
 @NgModule({
   declarations: [
     AppComponent,
-    VideoMakerComponent,
     VideoMakerPageComponent,
     ResultPageComponent,
     ResultVideoComponent,
